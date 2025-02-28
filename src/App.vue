e02b092b3a05f03104c89545c2f220c73b1f32d8
Muhammadzohid08 <boronovmuhammadzohid50@gmail.com>
2025-02-28 15:51:08 +0500

    starting the uzmovi clone

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import NavigationVue from "./components/Navigation.vue";
+</script>
+
+<template>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <NavigationVue />
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    class="w-full h-[105px] flex justify-center items-center bg-[#222933] px-2"
+  >
+    <div class="container w-[77%] flex justify-between items-center">
+      <img src="../../public/logo.gif" alt="" />
+      <div class="w-full h-[105px] flex justify-center items-center ml-24">
+        <div style="transition: 0.5s all ease" class="w-auto px-2 border-b-[4px] border-[#7F4726] h-full flex justify-center items-center hover:bg-[#7F4726]">
+            <h2 class="uppercase text-[#99BBDC] text-[15px]">Boshqa Sahifa</h2>
+        </div>
+        <div style="transition: 0.5s all ease" class="w-auto px-2 h-full flex justify-center border-b-[4px] border-[#2A6C4E] items-center hover:bg-[#2A6C4E]">
+            <h2 class="uppercase text-[#99BBDC] text-[15px]">kinolar <i class="fa-solid fa-caret-down ml-2"></i></h2>
+        </div>
+        <div style="transition: 0.5s all ease" class="w-auto px-2 h-full flex justify-center border-b-[4px] border-[#1A637E] items-center hover:bg-[#1A637E]">
+            <h2 class="uppercase text-[#99BBDC] text-[15px]">seriallar</h2>
+        </div>
+        <div style="transition: 0.5s all ease" class="w-auto px-2 h-full flex justify-center border-b-[4px] border-[#7E273C] items-center hover:bg-[#7E273C]">
+            <h2 class="uppercase text-[#99BBDC] text-[15px]">janr <i class="fa-solid fa-caret-down ml-1"></i></h2>
+        </div>
+        <div style="transition: 0.5s all ease" class="w-auto px-2 h-full flex justify-center border-b-[4px] border-[#7F6A3F] items-center hover:bg-[#7F6A3F]">
+            <h2 class="uppercase text-[#99BBDC] text-[15px]">yil <i class="fa-solid fa-caret-down ml-1"></i></h2>
+        </div>
+        <div style="transition: 0.5s all ease" class="w-auto px-2 h-full flex justify-center border-b-[4px] border-[#553E6A] items-center hover:bg-[#553E6A]">
+            <h2 class="uppercase text-[#99BBDC] text-[15px]">mamlakat <i class="fa-solid fa-caret-down ml-1"></i></h2>
+        </div>
+        <div style="transition: 0.5s all ease" class="w-auto px-2 h-full flex justify-center border-b-[4px] border-[#ff690766] items-center hover:bg-[#ff690766]">
+            <h2 class="uppercase text-[#99BBDC] text-[15px]">kino yangiliklari</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
